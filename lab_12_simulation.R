aac5dd59367a03aef3530c45e4184e73452ee63b
Haoyue Liang <sherryliang@Haoyues-MBP.wv.cc.cmu.edu>
2018-08-03 12:51:47 -0400

    question 2b

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,12 @@ generate_data <- function(n, p) {
   return (list(covariates,responses))
 }
 generate_data(3,4)
+
+
+model_select <- function(covariates, responses, cutoff) {
+  result = lm(responses ~ covariates)
+  new.covariates = covariates[,which(summary(result)$coefficients[-1,4] <= cutoff)]
+  if (length(new.covariates)== 0) {return (vector(mode="numeric", length=0))}
+  result2 = lm(responses ~ new.covariates)
+  return (summary(result2)$coefficients[-1,4])
+}
